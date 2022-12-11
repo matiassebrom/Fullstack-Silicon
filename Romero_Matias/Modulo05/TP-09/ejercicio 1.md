c7fa997ae8e9aaf998a414d46db1e8515558faab
Matias <matiassebrom@gmail.com>
2022-12-11 18:20:20 -0300

    agrego los ejercicios del tp-10

---
+++ Romero_Matias/Modulo05/TP-09/ejercicio 1.md
@@ -0,0 +1,14 @@
+Ejercicio 1
+Investigar y dar una definición con sus palabras los siguientes términos:
+REST
+API
+Endpoint
+Encabezados (headers)
+
+REST (Representational State Transfer) es un estilo de arquitectura de software que se utiliza para diseñar servicios web y APIs (interfaces de programación de aplicaciones). La arquitectura REST se basa en un conjunto de principios y convenciones que se utilizan para estructurar los servicios web y hacerlos más accesibles y fáciles de utilizar.
+
+Una API (Application Programming Interface) es un conjunto de protocolos y herramientas que se utilizan para construir aplicaciones y permiten a los desarrolladores acceder a ciertos servicios o funcionalidades de un sistema de software. Las APIs son esenciales para la interoperabilidad entre diferentes sistemas y plataformas, ya que permiten a las aplicaciones interactuar y compartir datos de manera sencilla y eficiente.
+
+Un endpoint (punto final) es una URL específica que se utiliza para acceder a un recurso o servicio en una API. Los endpoints son la forma en que las aplicaciones cliente interactúan con una API y pueden ser utilizados para enviar y recibir datos, realizar operaciones, etc.
+
+Los encabezados (headers) son campos de información que se incluyen en las peticiones y respuestas HTTP para proporcionar información adicional sobre el contenido de la solicitud o la respuesta. Los encabezados pueden ser utilizados para especificar información como el tipo de contenido, la autenticación, la localización, entre otros. Esta información se utiliza por el servidor y el cliente para procesar la solicitud y generar una respuesta adecuada.
---
+++ Romero_Matias/Modulo05/TP-09/ejercicio 3/alpha-silicon-01_materias.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: alpha-silicon-01
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `materias`
+--
+
+DROP TABLE IF EXISTS `materias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materias` (
+  `idmateria` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `objetivo` text,
+  `plan_estudio` text,
+  `estado` char(1) DEFAULT '1',
+  PRIMARY KEY (`idmateria`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materias`
+--
+
+LOCK TABLES `materias` WRITE;
+/*!40000 ALTER TABLE `materias` DISABLE KEYS */;
+INSERT INTO `materias` VALUES (14,'node','objetivo node','plan de estudio node','1'),(15,'Javascriot','objetivo node','plan de estudio node','1'),(16,'backenc','objetivo backend','plan de estudio backend','1'),(17,'frontend','objetivo frontend','plan de estudio frontend','1'),(18,'ingles 1','objetivo ingles 1','plan de estudio ingles 1','1'),(19,'ingles 2','objetivo ingles 2','plan de estudio ingles 2','1');
+/*!40000 ALTER TABLE `materias` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-11 18:09:16
